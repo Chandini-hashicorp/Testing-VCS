0d831091a89e7098b9944f6b26b76b232a0d088e
Chandini <chandini.nln@hashicorp.com>
2026-03-25 09:13:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 terraform {
-  required_providers {
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
-    }
-  }
+  required_providers {
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
 }
 
 provider "time" {}
 
 locals {
-  items = toset([for i in range(10) : tostring(i)])
+  items = toset([for i in range(10) : tostring(i)])
 }
 
 resource "null_resource" "test" {
-  for_each = local.items
+  for_each = local.items
 }
 
 resource "time_sleep" "wait_after_each" {
-  for_each = null_resource.test
+  for_each = null_resource.test
 
-  create_duration = "5m"
+  create_duration = "5m"
 }
